c9ec4be0639032c7061cb962932b843fe1226788
akcinar.omer <akcinardev@gmail.com>
2024-07-31 10:56:18 +0200

    Add project files.

---
+++ EcommerceApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EcommerceApi.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
---
+++ EcommerceApi/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EcommerceApi.Models
+{
+	[Table("Products")]
+	public class Product
+	{
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+		public double Price { get; set; }
+        public string Category { get; set; } = string.Empty;
+        public int StockAmount { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
