54ac3d9e58220f3d0f95c6e3720c3de51268b119
akcinar.omer <akcinardev@gmail.com>
2024-08-06 14:42:25 +0200

    basic user management for admin added UserController

---
+++ EcommerceApi/Controllers/UserController.cs
@@ -0,0 +1,88 @@
+﻿using EcommerceApi.Data;
+using EcommerceApi.Dtos.Account;
+using EcommerceApi.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcommerceApi.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<AppUser> _userManager;
+
+        public UserController(ApplicationDbContext context, UserManager<AppUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var users = await _context.Users.ToListAsync();
+
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(RegisterAccountDto registerDto)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest();
+                }
+
+                var user = new AppUser
+                {
+                    UserName = registerDto.Username,
+                    Email = registerDto.EmailAddress
+                };
+
+                var createdUser = await _userManager.CreateAsync(user, registerDto.Password);
+
+                if (createdUser.Succeeded)
+                {
+                    return Ok(user);
+                }
+                else
+                {
+                    return StatusCode(500, createdUser.Errors);
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var existingUser = await _context.Users.FindAsync(id);
+
+            try
+            {
+                await _userManager.DeleteAsync(existingUser);
+                return Ok(existingUser);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
